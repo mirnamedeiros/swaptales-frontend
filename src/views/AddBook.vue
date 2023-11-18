c25a5dbecc6044eb6e455c78d5fc8b84c6cefda6
Mirna Medeiros <mirnamedeiros@pop-os.localdomain>
2023-11-18 19:37:19 -0300

    added select and file input to book form

--- src/views/AddBook.vue
+++ src/views/AddBook.vue
@@ -5,40 +5,37 @@
             <div class="mx-auto w-25 " style="max-width:100%;">
               <h2 class="text-center mb-3">Adicionar livro</h2>
               <form @submit.prevent="addBook">
-                <!--Title-->
-                <div class="row">
-                  <div class="col-md-12 form-group mb-3">
-                    <label for="title" class="form-label">Titulo</label>
-                    <input id="title"  type="text" name="title" class="form-control" placeholder="Insira o titulo" required v-model="book.title">
-                  </div>
+                <div class="col-md-12 form-group mb-3">
+                  <label for="title" class="form-label">Titulo</label>
+                  <input id="title"  type="text" name="title" class="form-control" placeholder="Insira o titulo" required v-model="book.title">
                 </div>
 
-                
-                <!--Author-->
-                <div class="row">
-                  <div class="col-md-12 form-group mb-3">
-                    <label for="author" class="form-label">Autor</label>
-                    <input id="author" type="text"  name="author" class="form-control" placeholder="Insira o autor" required v-model="book.author" >
-                  </div>
+                <div class="col-md-12 form-group mb-3">
+                  <label for="author" class="form-label">Autor</label>
+                  <input id="author" type="text"  name="author" class="form-control" placeholder="Insira o autor" required v-model="book.author" >
                 </div>
-               
-                <div class="row">
-                  <div class="col-md-12 form-group mb-3">
-                    <label for="edition" class="form-label">Edição</label>
-                    <input id="edition" type="text"  name="edition" class="form-control" placeholder="Insira a edição" required v-model="book.edition" >
-                  </div>
+              
+                <div class="col-md-12 form-group mb-3">
+                  <label for="edition" class="form-label">Edição</label>
+                  <input id="edition" type="text"  name="edition" class="form-control" placeholder="Insira a edição" required v-model="book.edition" >
                 </div>
-                
-               
-                <div class="row">
-                  <div class="col-md-12 form-group">
-                    <input class="btn btn-primary w-100" style="font-size: 1.2rem;" type="submit" value="Submit">
-                  </div>
+
+              <label for="edition" class="form-label">Objetivo</label>
+              <select class="form-select mb-3" aria-label=".form-select-lg example">
+                <option value="1" selected>Venda</option>
+                <option value="2">Troca</option>
+                <option value="3">Empréstimo</option>
+                <option value="4">Troca e Venda</option>
+              </select>
+
+              <div class="col-md-12 form-group mb-3">
+                <input id="file" type="file" accept="image/*"> 
+              </div>
+
+                <div class="col-md-12 form-group">
+                  <input class="btn btn-primary w-100" style="font-size: 1.2rem;" type="submit" value="Submit">
                 </div>
-      
-                <div>
-                  
-                </div>
+
               </form>
         
             </div>
