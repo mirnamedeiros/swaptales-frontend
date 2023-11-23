8c6f3806568f6bd3703dfa2ccc36fb1334d494a6
humberto7891 <humberto.neto.711@ufrn.edu.br>
2023-11-23 09:10:45 -0300

    termino da criacao do review

--- src/views/AddReview.vue
+++ src/views/AddReview.vue
@@ -1,9 +1,137 @@
+<script>
+  import NavBar from '../components/Navbar.vue'
+  import Footer from '../components/Footer.vue'
+  import { format } from 'date-fns';
+
+  export default {
+      name: 'AddReview',
+      components: {
+        NavBar, 
+        Footer
+      },
+
+      data() {
+          return {
+              review : {
+                idAuthorReview: localStorage.getItem('currentUser'),
+                idBook: this.$route.params.id,
+                reviewDate: format(new Date(), 'MM-dd-yyyy'),
+                text: '',
+                stars: 5,
+              },
+              book: { },
+          }
+      },
+      mounted(){
+        this.findBook(this.review.idBook);
+      },
+      methods: {
+        onFileChange(event) {
+          this.file = event.target.files[0];
+        },
+
+        addReview(){
+
+            fetch('http://localhost:8080/swaptales/api/reviews', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(this.review),
+            })
+                .then(response => {
+                if (response.status === 201) {
+                    console.log('Livro adicionado com sucesso');
+                    this.$router.push('/books');
+                } else {
+                    console.error('Erro ao adicionar review:', response.statusText);
+                }
+                })
+                .catch(error => {
+                console.error('Erro ao fazer a solicitação:', error);
+                });
+        },
+        findBook(id){
+				fetch(`http://localhost:8080/swaptales/api/books/${id}`, {
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					method: 'GET',
+				})
+					.then(response => {
+						if (!response.ok) {
+							throw new Error(`Erro ao recuperar o usuario: ${response.statusText}`);
+						}
+						return response.text();
+					})
+					.then(data => {
+						if(data){
+							this.setBook(JSON.parse(data));
+						}else{
+							console.log("Usuario não encontrado");
+						}
+					})
+					.catch(error => {
+						console.error('Erro ao fazer a solicitação para a api de usuarios:', error);
+					});
+        	},
+        setBook(data){
+            this.book.title = data.title;
+            this.book.price = (data.price) ? data.price : 0;
+            this.book.countReview = (data.reviews) ? data.reviews.length : 0;
+            this.book.idioma = (data.idioma)? data.idioma : '-';
+            this.book.author = data.author;
+            this.book.description = (data.description) ? data.description : '-';
+            this.book.urlImg = data.urlImg;
+            this.book.availabilityStatus = data.availabilityStatus;
+            this.book.countDaysLoan = data.countDaysLoan;
+            this.idUserBook = data.ownerUser.id;
+        },
+      },
+          
+  }
+</script>
+
+<style lang="scss" scoped>
+    @import url('../assets/scss/style.scss');
+    @import url('../assets/scss/form-login.scss');
+</style>
+
 <template>
     <main>
       <NavBar/>
         <div class="my-5">
-            <div class="mx-auto w-25 " style="max-width:100%;">
-              <h2 class="text-center mb-3">Adicionar Análise</h2>
+            <h1 class="text-center mb-3">Adicionar Análise</h1>
+
+            <div class="d-flex justify-content-center">
+                <div class="card-profile p-5">
+                    <div class="container-fliud">
+                        <div class="wrapper row">
+                            <div class="preview col-md-5">
+                                <div class="preview-pic tab-content">
+                                    <div class="tab-pane active" id="pic-1"><img :src="book.urlImg" width="155"/></div>
+                                </div>
+                            </div>
+                            <div class="details col-md-7">
+                                <h3 class="product-title">{{ book.title }}</h3>
+                                <div class="rating">
+                                    <span class="review-no">{{ book.countReview }} reviews</span>
+                                </div>
+                                <h5 class="titles">Linguagem:
+                                    <span>{{ book.idioma }}</span>
+                                </h5>
+                                <h5 class="titles">Escritor(a):
+                                    <span>{{ book.author }}</span>
+                                </h5>
+
+                                <p class="product-description">{{ book.description }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mx-auto w-25 mt-5 " style="max-width:100%;">
               <form @submit.prevent="addReview">
 
                 <div class="col-md-12 form-group mb-3">
@@ -30,64 +158,3 @@
     </main>
     <!-- <Footer/> -->
 </template>
-
-
-<script>
-  import NavBar from '../components/Navbar.vue'
-  import Footer from '../components/Footer.vue'
-  import { format } from 'date-fns';
-
-  export default {
-      name: 'AddReview',
-      components: {
-        NavBar, 
-        Footer
-      },
-
-      data() {
-          return {
-              review : {
-                idAuthorReview: localStorage.getItem('currentUser'),
-                idBook: this.$route.params.id,
-                reviewDate: format(new Date(), 'MM-dd-yyyy'),
-                text: '',
-                stars: 5,
-              },
-          }
-      },
-
-      methods: {
-        onFileChange(event) {
-          this.file = event.target.files[0];
-        },
-
-        addReview(){
-
-            fetch('http://localhost:8080/swaptales/api/reviews', {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(this.review),
-            })
-                .then(response => {
-                if (response.status === 201) {
-                    console.log('Livro adicionado com sucesso');
-                    this.$router.push('/books');
-                } else {
-                    console.error('Erro ao adicionar review:', response.statusText);
-                }
-                })
-                .catch(error => {
-                console.error('Erro ao fazer a solicitação:', error);
-                });
-        }
-      },
-          
-  }
-</script>
-
-<style lang="scss" scoped>
-    @import url('../assets/scss/style.scss');
-    @import url('../assets/scss/form-login.scss');
-</style>
